6c32a0495be8af673308e715aae7fa652137346a
ninaksrs <ninaksrs@gmail.com>
2025-10-21 00:38:28 +0700

    LabsProject-MCU

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  Serial.println("on");           
+  delay(1000);            
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  Serial.println("off");          
+  delay(1000);                       // wait for a second
+}
